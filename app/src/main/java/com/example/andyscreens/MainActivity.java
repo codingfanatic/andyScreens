19339af0f5b6b611954926fde49da3f469881cd6
richard-remote <clarkeril8082@gmail.com>
2019-07-08 16:05:28 -0400

    Three Activities Added three activities and all of them can talk to each other OHO!

--- app/src/main/java/com/example/andyscreens/MainActivity.java
+++ app/src/main/java/com/example/andyscreens/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.andyscreens;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.text);
+
+    }
+
+    public void Ganesh(View View)
+    {
+        String button_text;
+        button_text =((Button)View).getText().toString();
+        if(button_text.equals("click second activity"))
+        {
+            Intent ganesh = new Intent(this,SecondActivity.class);
+            startActivity(ganesh);
+        }
+        else if (button_text.equals("click third activity"))
+        {
+            Intent mass = new Intent(this,ThirdActivity.class);
+            startActivity(mass);
+
+        }
     }
 }
---
+++ app/src/main/java/com/example/andyscreens/SecondActivity.java
@@ -0,0 +1,18 @@
+package com.example.andyscreens;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by For on 4/14/2017. 
+ */
+
+public class SecondActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+    }
+}   
