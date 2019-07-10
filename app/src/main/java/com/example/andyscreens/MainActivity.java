769ba642ac7c73a0536cfbb7c7f66067e9780980
richard-remote <clarkeril8082@gmail.com>
2019-07-10 16:27:07 -0400

    onClick attribute android:onclick attritbute calls a function defined in the corresponding Class file -Dunlaps is the name of the function defined in Mainactivity.java -Dunlaps instantiates an Intent before calling startActivity() and passing the Intent object as an  argument

--- app/src/main/java/com/example/andyscreens/MainActivity.java
+++ app/src/main/java/com/example/andyscreens/MainActivity.java
@@ -6,20 +6,17 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = (TextView) findViewById(R.id.text);
 
     }
 
-    public void Ganesh(View View) {
+    public void Dunlaps(View View) {
         String button_text;
 
         //Casts the View object as a Button
