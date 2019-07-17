938baf17f68473fc7da24ea60a20ea06b20f76d6
richard-remote <clarkeril8082@gmail.com>
2019-07-17 17:24:43 -0400

    Added arrows with no functionality Arrows are now in the action bars. However, clicking them doesn't do anything...yet

--- app/src/main/java/com/example/andyscreens/SecondActivity.java
+++ app/src/main/java/com/example/andyscreens/SecondActivity.java
@@ -3,13 +3,18 @@ package com.example.andyscreens;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 
-public class SecondActivity extends Activity {
+
+public class SecondActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-    }
 
-    
-}   
+        getSupportActionBar().setTitle("Act II");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+    }
+}
